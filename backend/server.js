82c61175d737a17ef35d4188e95186c075878a66
Karl <118022278+Hannya4k@users.noreply.github.com>
2025-03-28 23:25:28 +0800

    add product creation endpoint with validation

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/products", (req, res) => {
-  res.send("Server is ready");
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name | !product.price | !product.image) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("this is error", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 app.listen(5000, () => {
